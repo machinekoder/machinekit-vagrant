18a617160548aa0dae7874d060eb84c674385f73
Alexander Rössler <mail.aroessler@gmail.com>
2015-11-02 21:34:25 -0600

    cloning mkwrapper-sim and setting REMOTE=1

--- cookbook/machinekit-package.sh
+++ cookbook/machinekit-package.sh
@@ -19,3 +19,24 @@ else
     #sudo apt-get upgrade -y
     sudo apt-get install -y machinekit-posix avahi-daemon
 fi
+
+if [ -e ./repos/mkwrapper-sim ]; then
+    echo "Mkwrapper-sim already installed"
+else
+    echo "Cloning mkwrapper-sim"
+    cd /home/vagrant
+    mkdir -p repos
+    cd repos
+    git clone https://github.com/strahlex/mkwrapper-sim
+    chown -R vagrant mkwrapper-sim
+fi
+
+if grep -Fxq "REMOTE=1" /etc/linuxcnc/machinekit.ini; then
+    echo "Machinetalk remote already enabled"
+else
+    echo "Enabling Machinetalk remote"
+    apt-get install -y uuid-runtime
+    sed -i 's/REMOTE=.*/REMOTE=1/' /etc/linuxcnc/machinekit.ini
+    UUID=`uuidgen`
+    sed -i "s/MKUUID=.*/MKUUID=$UUID/" /etc/linuxcnc/machinekit.ini
+fi
