8da90809c936f8c7ce1bf12c6ada20c9f6a3359a
Alexander Rössler <mail.aroessler@gmail.com>
2016-04-10 20:05:16 +0200

    Using VirtualBox Guest additions packages

--- cookbook/vbox-guest-additions.sh
+++ cookbook/vbox-guest-additions.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
-VBOX_VERSION=`ls /opt/`
-if [[ "$VBOX_VERSION" == *"VBoxGuestAdditions-5.0.6"* ]]; then
-    echo "VBox Guest Additions up to date"
+if [ $(query-package virtualbox-guest-x11) -eq 1 ]; then
+    echo "VirtualBox Guest Additions already installed"
+    #sudo apt-get update
+    #sudo apt-get upgrade -y
 else
-    echo "Updating VBox Guest Additions"
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y build-essential module-assistant
-m-a prepare -i
-cd /opt
-echo "Downloading VBoxGuestAdditions"
-wget -c http://download.virtualbox.org/virtualbox/5.0.6/VBoxGuestAdditions_5.0.6.iso -O VBoxGuestAdditions.iso -nv
-mount VBoxGuestAdditions.iso -o loop /mnt
-cd /mnt
-sh VBoxLinuxAdditions.run
-cd /opt
-rm *.iso
+    sudo sh -c \
+    "echo 'deb http://debian.inode.at/debian jessie-backports main contrib non-free' > \
+    /etc/apt/sources.list.d/backports.list"
+    sudo apt-get update
+    #sudo apt-get upgrade -y
+    sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
 fi
+
