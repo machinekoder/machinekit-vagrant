743747601efd1e5be3a59a0ad787f9429570b630
Alexander Rössler <mail@roessler.systems>
2017-02-25 13:37:22 +0100

    fix guest additions installation

--- cookbook/vbox-guest-additions.sh
+++ cookbook/vbox-guest-additions.sh
@@ -8,11 +8,10 @@ query-package () {
 if [ $(query-package virtualbox-guest-x11) -eq 1 ]; then
     echo "VirtualBox Guest Additions already installed"
 else
-    sudo sh -c \
+    sh -c \
     "echo 'deb http://debian.inode.at/debian jessie-backports main contrib non-free' > \
     /etc/apt/sources.list.d/backports.list"
-    sudo apt-get update
-    #sudo apt-get upgrade -y
-    sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
+    apt-get update
+    apt-get install linux-headers-$(uname -r)
+    apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms -t jessie-backports
 fi
-
